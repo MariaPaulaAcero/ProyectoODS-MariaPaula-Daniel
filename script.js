5e2abcc2e02708d0b9dc0f1775f212b19d86ec51
MrSanx <sanchezestudios3111@gmail.com>
2023-10-07 12:26:08 -0500

    galeria

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+const images = document.querySelectorAll('.gallery-image');
+let currentIndex = 0;
+
+function nextImage() {
+    images[currentIndex].style.opacity = '0';
+    currentIndex = (currentIndex + 1) % images.length;
+    images[currentIndex].style.opacity = '1';
+}
+
+setInterval(nextImage, 3000); // Change image every 3 seconds (adjust as needed)
